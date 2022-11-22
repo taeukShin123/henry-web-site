77c98d94d076ced4a4671138f54fcbde5f8ec529
Henry Shin <shintaeuk91@gmail.com>
2022-11-23 01:22:25 +0900

    test

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 var width = window.innerWidth,
-height = window.innerHeight;
+    height = window.innerHeight;
 
 // Create a renderer and add it to the DOM.
 var renderer = new THREE.WebGLRenderer();
@@ -15,7 +15,7 @@ scene.add(camera);
 
 // Create a light, set its position, and add it to the scene.
 var light = new THREE.PointLight(0xffffff);
-light.position.set(-100, 200, 100);
+light.position.set(-100,200,100);
 scene.add(light);
 
 // Add OrbitControls so that we can pan around with the mouse.
@@ -23,23 +23,23 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // Add axes
 var axes = new THREE.AxisHelper(50);
-scene.add(axes);
+scene.add( axes );
 
-var geometry = new THREE.BoxGeometry(5, 5, 5);
-var cubeMaterial = new THREE.MeshNormalMaterial();
+var geometry = new THREE.BoxGeometry(5,5,5);
+var cubeMaterial = new THREE.MeshNormalMaterial(); 
 
-var mesh = new THREE.Mesh(geometry, cubeMaterial);
-scene.add(mesh);
+var mesh = new THREE.Mesh( geometry, cubeMaterial );
+scene.add( mesh );
 
 resize();
 animate();
-window.addEventListener('resize', resize);
+window.addEventListener('resize',resize);
 
-function resize() {
+function resize(){
   let w = window.innerWidth;
   let h = window.innerHeight;
-
-  renderer.setSize(w, h);
+  
+  renderer.setSize(w,h);
   camera.aspect = w / h;
   camera.updateProjectionMatrix();
 }
@@ -47,8 +47,8 @@ function resize() {
 // Renders the scene
 function animate() {
 
-  renderer.render(scene, camera);
+  renderer.render( scene, camera );
   controls.update();
 
-  requestAnimationFrame(animate);
+  requestAnimationFrame( animate );
 }
